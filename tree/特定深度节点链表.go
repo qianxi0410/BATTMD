c2c01dda94909cbc6221883bd2967eb397043785
qianxi <894871277@qq.com>
2021-03-07 22:37:14 +0800

    day08

---
+++ tree/特定深度节点链表.go
@@ -0,0 +1,48 @@
+package tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func listOfDepth(tree *TreeNode) []*ListNode {
+	res := make([]*ListNode, 0)
+	queue := make([]*TreeNode, 0)
+
+	if tree == nil {
+		return res
+	}
+
+	queue = append(queue, tree)
+	for len(queue) > 0 {
+		size := len(queue)
+		virtualHead := &ListNode{}
+		pre := virtualHead
+		for i := 0; i < size; i++ {
+			top := queue[0]
+			queue = queue[1:]
+			node := &ListNode{
+				Val:  top.Val,
+				Next: nil,
+			}
+			pre.Next = node
+			pre = pre.Next
+
+			if top.Left != nil {
+				queue = append(queue, top.Left)
+			}
+			if top.Right != nil {
+				queue = append(queue, top.Right)
+			}
+		}
+		res = append(res, virtualHead.Next)
+	}
+
+	return res
+}
