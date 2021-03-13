f0b4fd0cf868fc70e84f8e7d93abb01cf9d46450
千禧 <55543743+qianxi0410@users.noreply.github.com>
2021-03-13 09:54:30 +0800

    Update 最小高度树.go

--- tree/最小高度树.go
+++ tree/最小高度树.go
@@ -2,6 +2,10 @@ package tree
 
 import "sort"
 
+/**
+https://leetcode-cn.com/problems/minimum-height-tree-lcci/comments/
+*/
+
 func build(nums []int, left, right int) *TreeNode {
 	if left > right {
 		return nil
