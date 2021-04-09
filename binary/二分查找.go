057f4768824b84cb866f6f1d156409af66efd128
qianxi0410 <894871277@qq.com>
2021-04-09 08:48:14 +0800

    day36

---
+++ binary/二分查找.go
@@ -0,0 +1,20 @@
+package binary
+
+/**
+https://leetcode-cn.com/problems/binary-search/comments/
+ */
+func search2(nums []int, target int) int {
+	l, r := 0, len(nums) - 1
+
+	for l < r {
+		mid := l + (r - l) >> 1
+		if nums[mid] < target {
+			l = mid + 1
+		} else if nums[mid] > target {
+			r = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return -1
+}
