1fb5bfb0e70eb42cb251d12996405bd22d377a0d
qianxi0410 <894871277@qq.com>
2021-05-27 10:02:57 +0800

    day 74

---
+++ hot100/206. 反转链表.go
@@ -0,0 +1,14 @@
+package hot100
+
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+
+	for cur != nil {
+		tmp := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = tmp
+	}
+	return pre
+}
