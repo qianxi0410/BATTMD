e6dc7fc29ca03d11d46d91169eabb02f9233e4fa
qianxi0410 <894871277@qq.com>
2021-06-30 11:22:15 +0800

    day 107

---
+++ hot100/221. 最大正方形.go
@@ -0,0 +1,22 @@
+package hot100
+
+func maximalSquare(matrix [][]byte) int {
+	res := 0
+
+	row, col := len(matrix), len(matrix[0])
+	if row < 1 {
+		return 0
+	}
+	dp := make([][]int, row+1)
+	for i := range dp {
+		dp[i] = make([]int, col+1)
+	}
+
+	for i := 1; i <= row; i++ {
+		for j := 1; j <= col; j++ {
+			dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]))
+			res = max(res, dp[i][j])
+		}
+	}
+	return res * res
+}
